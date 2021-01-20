ff1b3ff10b332e2e702016f63e66f334b24afe9b
Matthew_Cash <matthew@matthew-cash.com>
2021-01-19 16:33:31 -0800

    Initial Commit

---
+++ actions.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import { deviceStateListner } from './main';
+
+// LED Tree
+deviceStateListner.on('0af5d793-8006-4cad-a19d-384a95b3c09a', value => {
+    axios.post(process.env.CONTROL_URL, {
+        usb: value
+    });
+});
+
+// Computer
+deviceStateListner.on('cf3c2ecd-2c62-4a74-8078-fb0a01540354', value => {
+    axios.post(process.env.CONTROL_URL, {
+        computer: value
+    });
+});
---
+++ store.ts
@@ -0,0 +1,49 @@
+import { SmartThingsDevice } from './schema';
+
+export class ContextStore {
+    constructor() {}
+    async get(installedAppId) {
+        return SmartThingsDevice.findOne({ installedAppId: installedAppId });
+    }
+
+    async put(params) {
+        await SmartThingsDevice.deleteOne({
+            installedAppId: params.installedAppId
+        });
+
+        const data = new SmartThingsDevice({
+            installedAppId: params.installedAppId,
+            locationId: params.locationId,
+            authToken: params.authToken,
+            refreshToken: params.refreshToken,
+            clientId: params.clientId,
+            clientSecret: params.clientSecret,
+            config: params.config
+        });
+
+        return data.save();
+    }
+
+    async update(installedAppId, params) {
+        const data = await SmartThingsDevice.findOne({
+            installedAppId: installedAppId
+        });
+
+        data.authToken = params.authToken;
+        data.refreshToken = params.refreshToken;
+
+        return data.save();
+    }
+
+    async delete(installedAppId) {
+        return SmartThingsDevice.remove({ installedAppId: installedAppId });
+    }
+
+    static async getDevices(installedAppId) {
+        const context = await SmartThingsDevice.findOne({
+            installedAppId: installedAppId
+        });
+
+        return context.config.switch;
+    }
+}
