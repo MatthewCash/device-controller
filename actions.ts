68f8450a78d438865c2de82c776d8cce278d693b
Matthew_Cash <matthew@matthew-cash.com>
2021-03-27 12:20:06 -0700

    Log errors when sending actions

--- actions.ts
+++ actions.ts
@@ -3,14 +3,24 @@ import { deviceStateListner } from './devices';
 
 // LED Tree
 deviceStateListner.on('0af5d793-8006-4cad-a19d-384a95b3c09a', value => {
-    axios.post(process.env.CONTROL_URL, {
-        usb: value
-    });
+    axios
+        .post(process.env.CONTROL_URL, {
+            usb: value
+        })
+        .catch(error => {
+            console.warn(`An error occured while setting LED Tree to ${value}`);
+            console.error(error);
+        });
 });
 
 // Computer
 deviceStateListner.on('cf3c2ecd-2c62-4a74-8078-fb0a01540354', value => {
-    axios.post(process.env.CONTROL_URL, {
-        computer: value
-    });
+    axios
+        .post(process.env.CONTROL_URL, {
+            computer: value
+        })
+        .catch(error => {
+            console.warn(`An error occured while setting Computer to ${value}`);
+            console.error(error);
+        });
 });
